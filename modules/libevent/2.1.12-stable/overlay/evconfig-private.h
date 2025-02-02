e56243047c76efe8fd35950748205eb1e19e9a12
Derek Mauro <dmauro@google.com>
2025-02-02 16:03:26 -0500

    Add the Apple configuration

--- modules/libevent/2.1.12-stable/overlay/evconfig-private.h
+++ modules/libevent/2.1.12-stable/overlay/evconfig-private.h
@@ -3,6 +3,8 @@
 
 #if defined(__linux__)
 #include "evconfig-private_linux.h"
+#elif defined(__APPLE__)
+#include "evconfig-private_apple.h"
 #endif
 
 #endif  // EVCONFIG_PRIVATE_H_INCLUDED_
--- modules/libevent/2.1.12-stable/overlay/include/event2/event-config.h
+++ modules/libevent/2.1.12-stable/overlay/include/event2/event-config.h
@@ -3,6 +3,8 @@
 
 #if defined(__linux__)
 #include <event2/event-config_linux.h>
+#elif defined(__APPLE__)
+#include <event2/event-config_apple.h>
 #endif
 
 #endif  // EVENT2_EVENT_CONFIG_H_INCLUDED_
